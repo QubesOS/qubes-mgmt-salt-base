34284349e6076f932f7167edd55ffc4d09f73248
Jason Mehring <nrgaway@gmail.com>
2015-12-23 06:10:04 -0500

    modules: qubes - pylint

--- _modules/qubes.py
+++ _modules/qubes.py
@@ -12,6 +12,8 @@ Misc Helper Functions
 =====================
 '''
 
+from __future__ import absolute_import
+
 # Import python libs
 import collections
 import logging
@@ -25,21 +27,21 @@ def merge(target, source, create=False, allowed=None, append=False):
     Merges the values of a nested dictionary of varying depth without over-
     writing the targets root nodes.
 
-    target
-        Target dictionary to update
+    :param target:
+        Target dictionary to update.
 
-    source
-        Source dictionary that will be used to update `target`
+    :param source:
+        Source dictionary that will be used to update `target`.
 
-    create
-        if True then new keys can be created during update, otherwise they will be tossed
-        if they do not exist in `target`.
+    :param create:
+        If True then new keys can be created during update, otherwise they will
+        be tossed if they do not exist in `target`.
 
-    allowed
-        list of allowed keys that can be created even if create is False
+    :param allowed:
+        List of allowed keys that can be created even if create is False.
 
-    append [True] or ['list_of_keys', 'key4']
-        appends to strings or lists if append is True or key in list
+    :param append: [True] or ['list_of_keys', 'key4']
+        Appends to strings or lists if append is True or key in list.
     '''
     if not allowed:
         allowed = []
@@ -55,6 +57,7 @@ def merge(target, source, create=False, allowed=None, append=False):
                     append=append
                 )
                 target[key] = replace
+
         else:
             if key in target.keys() or create or key in allowed:
                 if append and (append is True or key in append):
@@ -63,13 +66,16 @@ def merge(target, source, create=False, allowed=None, append=False):
                     ):
                         target.setdefault(key, '')
                         target[key] += source[key]
+
                     elif isinstance(source[key], list) and isinstance(
                         target.get(key, []), list
                     ):
                         target.setdefault(key, [])
                         target[key].extend(source[key])
+
                     else:
                         target[key] = source[key]
+
                 else:
                     target[key] = source[key]
     return target
