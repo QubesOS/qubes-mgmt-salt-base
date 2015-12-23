3f9b2787bf57370779d84e89ed0abf6e5b628555
Jason Mehring <nrgaway@gmail.com>
2015-12-23 05:24:06 -0500

    modules: debug - reformatted

--- _modules/debug.py
+++ _modules/debug.py
@@ -43,6 +43,7 @@ def __virtual__():
 class _TestBase(_ModuleBase):
     '''Overrides.
     '''
+
     def __init__(self, *varargs, **kwargs):
         '''
         '''
@@ -72,12 +73,40 @@ def mode(*varargs, **kwargs):
     '''
     #self.arg_options_create(argv_ordering=['flags', 'args', 'varargs', 'keywords'])
     base = _TestBase('debug.mode', **kwargs)
-    base.parser.add_argument('--status-mode', nargs='*', default=['all'], choices=('last', 'all', 'debug', 'debug-changes'), help=argparse.SUPPRESS)
+    base.parser.add_argument(
+        '--status-mode',
+        nargs='*',
+        default=['all'],
+        choices=('last', 'all', 'debug', 'debug-changes'),
+        help=argparse.SUPPRESS
+    )
     base.parser.add_argument('id', nargs='?', help=argparse.SUPPRESS)
-    base.parser.add_argument('--enable-all', nargs='?', type=bool, default=False, help='Enable debug mode for all modules')
-    base.parser.add_argument('--disable-all', nargs='?', type=bool, default=False, help='Disable debug mode for all modules')
-    base.parser.add_argument('--enable', nargs='+', default=False, help='Enable debug mode for provided modules')
-    base.parser.add_argument('--disable', nargs='+', default=False, help='Disable debug mode for provided modules')
+    base.parser.add_argument(
+        '--enable-all',
+        nargs='?',
+        type=bool,
+        default=False,
+        help='Enable debug mode for all modules'
+    )
+    base.parser.add_argument(
+        '--disable-all',
+        nargs='?',
+        type=bool,
+        default=False,
+        help='Disable debug mode for all modules'
+    )
+    base.parser.add_argument(
+        '--enable',
+        nargs='+',
+        default=False,
+        help='Enable debug mode for provided modules'
+    )
+    base.parser.add_argument(
+        '--disable',
+        nargs='+',
+        default=False,
+        help='Disable debug mode for provided modules'
+    )
     args = base.parse_args(*varargs, **kwargs)
 
     # Enable
@@ -86,14 +115,20 @@ def mode(*varargs, **kwargs):
             base._debug_mode.append('__all__')
             base.save_status(message='Enabled \'ALL\'')
         else:
-            base.save_status(prefix='[SKIP] ', message='Already enabled \'ALL\'')
+            base.save_status(
+                prefix='[SKIP] ',
+                message='Already enabled \'ALL\''
+            )
     elif args.enable:
         for module in args.enable:
             if module not in base._debug_mode:
                 base._debug_mode.append(module)
                 base.save_status(message='Enabled \'{0}\''.format(module))
             else:
-                base.save_status(prefix='[SKIP] ', message='Already enabled \'{0}\''.format(module))
+                base.save_status(
+                    prefix='[SKIP] ',
+                    message='Already enabled \'{0}\''.format(module)
+                )
 
     # Disable
     if args.disable_all:
@@ -106,7 +141,10 @@ def mode(*varargs, **kwargs):
                 base._debug_mode.remove(module)
                 base.save_status(message='Disabled \'{0}\''.format(module))
             else:
-                base.save_status(prefix='[SKIP] ', message='Already disabled \'{0}\''.format(module))
+                base.save_status(
+                    prefix='[SKIP] ',
+                    message='Already disabled \'{0}\''.format(module)
+                )
 
     # Returns the status 'data' dictionary
     return base.status()
